bc41e5603bab2ffb3189e9819312c1167c8d7831
Marqzzs <maumarqz007@gmail.com>
2023-10-16 10:49:30 -0300

    a

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# Projeto-IMC
+# Projeto Calculadora de IMC
+
+## Descrição
+Este é um projeto simples de uma calculadora de Índice de Massa Corporal (IMC) com armazenamento local. Ele permite aos usuários calcular seu IMC, salvar os resultados e visualizá-los em uma tabela.
+
+![Exemplo de Interface](./assets/screenshot.png)
+
+## Como Usar
+1. Abra o arquivo HTML no seu navegador.
+2. Preencha o formulário com o nome, altura e peso.
+3. Clique no botão "Calcular" para calcular o IMC.
+4. Os resultados serão exibidos na tabela abaixo do formulário.
+5. Os dados dos usuários serão armazenados localmente, então você não perderá as informações após recarregar a página.
+
+## Screenshots
+Aqui estão algumas capturas de tela do projeto:
+
+1. ![Interface do Usuário](./assets/screenshot.png)
+2. ![Resultados da Calculadora](./assets/screenshot2.png)
+3. ![Exemplo de Armazenamento Local](./assets/screenshot3.png)
+
+## Contribuições
+Contribuições são bem-vindas! Sinta-se à vontade para abrir problemas (issues) ou enviar solicitações de pull (pull requests) para melhorar o projeto.
+
+## Links
+- [Repositório GitHub](https://github.com/Marqzzs/Projeto-IMC)
+- [GitHub Pages](https://seu-usuario.github.io/nome-do-repositorio/)
+
+## Licença
+Este projeto está licenciado sob a Licença MIT - consulte o arquivo [LICENSE](./LICENSE) para obter detalhes.
+
+---
+
+Feito por [Seu Nome](https://github.com/seu-usuario)
--- imc-javascript-main/js/script.js
+++ imc-javascript-main/js/script.js
@@ -1,44 +1,171 @@
-function calcular() {
-    event.preventDefault();
+const arrPessoas = []; // Array vazio para armazenar informações de pessoas
 
+if (localStorage.getItem("arrPessoas") !== null) {
+  arrPessoas = [];
+} else {
+  arrPessoas = JSON.parse(localStorage.getItem("arrPessoas"));
+}
+
+listarPessoas();
+
+// Função que é chamada quando o formulário é submetido
+function calcular(e) {
+  e.preventDefault(); // Impede o comportamento padrão do formulário (recarregar a página)
+
+  // Coleta os valores do nome, altura e peso do formulário
+  const nome = document.getElementById("nome").value.trim();
+  const altura = parseFloat(document.getElementById("altura").value);
+  const peso = parseFloat(document.getElementById("peso").value);
+
+  // Verifica se os valores são válidos
+  if (isNaN(altura) || isNaN(peso) || nome.length == 0) {
+    alert("Preencha todos os campos");
+    return;
+  }
+
+  // Calcula o IMC
+  const imc = calcularImc(altura, peso);
+  const situacao = retornaSituacao(imc);
+
+  // Cria um objeto "pessoa" com as informações coletadas
+  let pessoa = {};
+  pessoa.nome = nome;
+  pessoa.altura = altura;
+  pessoa.peso = pes// Array vazio para armazenar informações de pessoas
+  const arrPessoas = [];
+  
+  // Verifica se há dados salvos no localStorage
+  if (localStorage.getItem("arrPessoas") !== null) {
+    arrPessoas = JSON.parse(localStorage.getItem("arrPessoas"));
+  }
+  
+  // Chama a função para listar as pessoas
+  listarPessoas();
+  
+  // Função que é chamada quando o formulário é submetido
+  function calcular(e) {
+    e.preventDefault(); // Impede o comportamento padrão do formulário (recarregar a página)
+  
+    // Coleta os valores do nome, altura e peso do formulário
     const nome = document.getElementById("nome").value.trim();
     const altura = parseFloat(document.getElementById("altura").value);
     const peso = parseFloat(document.getElementById("peso").value);
-    
-    if (isNaN(altura) || isNaN(peso) || nome.length == 0){
-        alert("Preencha todos os campos")
-        return;
+  
+    // Verifica se os valores são válidos
+    if (isNaN(altura) || isNaN(peso) || nome.length == 0) {
+      alert("Preencha todos os campos");
+      return;
     }
-
-    //calcular o imc
+  
+    // Calcula o IMC
     const imc = calcularImc(altura, peso);
-    const situacao = retornaSituacao(imc)
-
-    console.log(nome);
-    console.log(altura);
-    console.log(peso);
-    console.log(imc);
-    console.log(situacao);
-}
-
-function calcularImc(altura, peso) {
+    const situacao = retornaSituacao(imc);
+  
+    // Cria um objeto "pessoa" com as informações coletadas
+    let pessoa = {};
+    pessoa.nome = nome;
+    pessoa.altura = altura;
+    pessoa.peso = peso;
+    pessoa.imc = imc;
+    pessoa.situacao = situacao;
+  
+    // Adiciona o objeto "pessoa" ao array "arrPessoas"
+    arrPessoas.push(pessoa);
+  
+    // Salva o array "arrPessoas" no armazenamento local (localStorage)
+    localStorage.setItem('arrPessoas', JSON.stringify(arrPessoas));
+  
+    // Exibe o array atualizado na tabela HTML
+    listarPessoas();
+  }
+  
+  // Função para calcular o IMC
+  function calcularImc(altura, peso) {
     return peso / Math.pow(altura, 2);
+  }
+  
+  // Função para retornar a situação com base no IMC
+  function retornaSituacao(imc) {
+    if (imc <= 18.5) {
+      return "Magreza Severa";
+    } else if (imc >= 18.6 && imc <= 24.99) {
+      return "Peso normal";
+    } else if (imc >= 25 && imc <= 29.99) {
+      return "Acima do peso";
+    } else if (imc >= 30 && imc <= 34.99) {
+      return "Obesidade I";
+    } else if (imc >= 35 && imc <= 39.99) {
+      return "Obesidade II (severa)";
+    } else {
+      return "Cuidado!!!";
+    }
+  }
+  
+  // Função para listar as informações das pessoas na tabela HTML
+  function listarPessoas() {
+    let template = "";
+    for (let i = 0; i < arrPessoas.length; i++) {
+      template += `<tr>
+                      <td>${arrPessoas[i].nome}</td>
+                      <td>${arrPessoas[i].altura}</td>
+                      <td>${arrPessoas[i].peso}</td>
+                      <td>${arrPessoas[i].imc.toFixed(2)}</td>
+                      <td>${arrPessoas[i].situacao}</td>
+                      </tr>`;
+    }
+  
+    // Atualiza a tabela HTML com as informações das pessoas
+    document.getElementById('cadastro').innerHTML = template;
+  }
+  o;
+  pessoa.imc = imc;
+  pessoa.situacao = situacao;
+
+  // Adiciona o objeto "pessoa" ao array "arrPessoas"
+  arrPessoas.push(pessoa);
+
+  // Salva o array "arrPessoas" no armazenamento local (localStorage)
+  localStorage.setItem('arrPessoas', JSON.stringify(arrPessoas));
+
+  // Exibe o array atualizado na tabela HTML
+  listarPessoas();
 }
 
+// Função para calcular o IMC
+function calcularImc(altura, peso) {
+  return peso / Math.pow(altura, 2);
+}
+
+// Função para retornar a situação com base no IMC
 function retornaSituacao(imc) {
-    //validar o imc
-    //4 minutos para fazer
-    if (imc <= 18.5) {
-        return 'Magreza Severa'
-    }else if(imc >= 18.5 && imc <= 24.99){
-        return 'Peso normal'
-    }else if(imc >= 25 && imc <= 29.99){
-        return 'Acima do peso'
-    }else if(imc >= 30 && imc <= 34.99){
-        return 'Obesidade I'
-    }else if(imc >= 35 && imc <= 39.99){
-        return 'Obesidade II (severa)'
-    }else{
-        return 'Cuidado!!!'
-    }
+  if (imc <= 18.5) {
+    return "Magreza Severa";
+  } else if (imc >= 18.6 && imc <= 24.99) {
+    return "Peso normal";
+  } else if (imc >= 25 && imc <= 29.99) {
+    return "Acima do peso";
+  } else if (imc >= 30 && imc <= 34.99) {
+    return "Obesidade I";
+  } else if (imc >= 35 && imc <= 39.99) {
+    return "Obesidade II (severa)";
+  } else {
+    return "Cuidado!!!";
+  }
+}
+
+// Função para listar as informações das pessoas na tabela HTML
+function listarPessoas() {
+  let template = "";
+  for (let i = 0; i < arrPessoas.length; i++) {
+    template += `<tr>
+                    <td>${arrPessoas[i].nome}</td>
+                    <td>${arrPessoas[i].altura}</td>
+                    <td>${arrPessoas[i].peso}</td>
+                    <td>${arrPessoas[i].imc.toFixed(2)}</td>
+                    <td>${arrPessoas[i].situacao}</td>
+                    </tr>`;
+  }
+
+  // Atualiza a tabela HTML com as informações das pessoas
+  document.getElementById('cadastro').innerHTML = template;
 }
